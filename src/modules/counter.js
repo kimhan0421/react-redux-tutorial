29088979a093a2e36a76ce0df29c7b5548e99f4a
kimhan0421 <kimhan0421@naver.com>
2020-10-12 21:52:43 +0900

    Counter component update

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,26 +1,25 @@
-const INCREASE = "counter/INCREASE";
-const DECREASE = "counter/DECREASE";
+const INCREASE = 'counter/INCREASE'
+const DECREASE = 'counter/DECREASE'
 
-export const increase = () => ({type: INCREASE});
-export const decrease = () => ({type: DECREASE});
+export const increase = () => ({ type: INCREASE })
+export const decrease = () => ({ type: DECREASE })
 
-const initialState ={
-    number: 0
-};
-
-function counter(state=initialState, action) {
-    switch (action.type){
-        case INCREASE :
-            return {
-                number: state.number +1
-            };
-        case DECREASE:
-            return {
-                number: state.number -1
-            };
-        defalut:
-        return state;
-    }
+const initialState = {
+  number: 0,
 }
 
-export default counter;
+function counter(state = initialState, action) {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      }
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      }
+      defalut: return state
+  }
+}
+
+export default counter
